407f1d945634144d58da1d3827e444e00e053d6b
MontseD <95097614+MontseD@users.noreply.github.com>
2025-10-04 01:28:24 +0000

    Act 2 No se puede prestar

--- test_int.py
+++ test_int.py
@@ -2,8 +2,16 @@ from library import Library
 from book import Book
 from user import User
 
+#if __name__ == "__main__":
+    #Library_t = Library()
+    #Library_t.add_book(Book("To Kill a Mockingbird", "Harper Lee", 1960, "ISBN002"))
+    #Book_t= Book("To Kill a Mockingbird", "Harper Lee", 1960, "ISBN002")
+    #Book_t.borrow()
+
 if __name__ == "__main__":
     Library_t = Library()
-    Library_t.add_book(Book("To Kill a Mockingbird", "Harper Lee", 1960, "ISBN002"))
-    Book_t= Book("To Kill a Mockingbird", "Harper Lee", 1960, "ISBN002")
+    Book_t = Book("Pride and Prejudice", "Jane Austen", 1813, "ISBN005")
+    Library_t.add_book(Book_t)
     Book_t.borrow()
+    Book_t.borrow()
+    
